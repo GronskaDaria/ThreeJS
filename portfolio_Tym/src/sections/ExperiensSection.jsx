15d311ba82dc91cd685eaa7b7c855f02f31c6304
Daria Gronska <gronskaadariaa@gmail.com>
2025-10-29 22:33:26 +0100

    add testimonials cards

--- portfolio_Tym/src/sections/ExperiensSection.jsx
+++ portfolio_Tym/src/sections/ExperiensSection.jsx
@@ -1,8 +1,27 @@
+import TitleHeader from "../components/TitleHeader.jsx";
+import {expCards} from "../constants/index.js";
+import GlowCard from "../components/GlowCard.jsx";
+
 const ExperiensSection = () => {
     return (
         <section id="experience" className="w-full md:mt-40 mt-20 section-padding xl-:px-0">
             <div className=" w-full h-full md:px-20 px-5">
-
+                <TitleHeader title="Professional Work Experience" sub="💼My professional work experience"/>
+                <div className="mt-32 relative " >
+                    <div className="relative z-50 xl:space-y-32 space-y-10">
+                            {expCards.map((card, index)=>(
+                                <div key={card.title} className="exp-card-wrapper">
+                                    <div className="xl:w-2/6">
+                                        <GlowCard card={card} index={index}>
+                                            <div>
+                                                <img src={card.imgPath} alt="exp-img" />
+                                            </div>
+                                        </GlowCard>
+                                    </div>
+                                </div>
+                            ))}
+                    </div>
+                </div>
             </div>
 
 
