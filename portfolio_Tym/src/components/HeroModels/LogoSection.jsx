037eb818b82ee40f952e78549d2f21f13e63b92f
Daria Aleksandrova <dariaaleksandrova0815@outlook.com>
2025-10-29 19:57:43 +0100

    add LogoSection

---
+++ portfolio_Tym/src/components/HeroModels/LogoSection.jsx
@@ -0,0 +1,27 @@
+import {logoIconsList} from "../../constants/index.js";
+
+const LogoSection = () => {
+    const LogoIcon = ({icon}) => {
+        return (
+            <div className="flex items-center gap-2">
+                <img src={icon.imgPath} alt={icon.name}/>
+                <span>{icon.name}</span>
+            </div>
+        )
+    }
+    return (
+        <div className="md:my-20 my-10 relative">
+            <div className="gradient-edge "/>
+            <div className="gradient-edge "/>
+
+            <div className="marquee h-52">
+                <div className="marquee-box md:gap-12 gap-5">
+                    {logoIconsList.map((icon)=>(
+                        <LogoIcon key={icon.name} icon={icon}/>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+export default LogoSection
